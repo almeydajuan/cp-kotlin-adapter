9cea7c9a7367bf58756983f2aca8dbfc1ab3a1c8
Juan Almeyda <juan.almeyda@goeuro.com>
2018-09-06 09:32:14 +0200

    Basic hybrid configuration

---
+++ src/main/kotlin/com/juanalmeyda/adapter/GoEuroConnectServerApplication.java
@@ -0,0 +1,13 @@
+package com.juanalmeyda.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoEuroConnectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoEuroConnectServerApplication.class, args);
+    }
+
+}
